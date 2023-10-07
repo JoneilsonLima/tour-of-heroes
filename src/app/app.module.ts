55e5b68de239b29ee56ab478b993727a68aa9ef6
joneilson <joneilson84@gmail.com>
2023-10-07 16:28:59 -0300

    Criando componente de dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,15 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { AppRoutingModule } from './app-routing.module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
